3a3ca379454ed4bb3b1351596e4f548cdbe33074
julian0808 <41093464+julian0808@users.noreply.github.com>
2024-02-15 08:02:52 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import preact from '@astrojs/preact';
+import netlify from '@astrojs/netlify/functions';
 
+// https://astro.build/config
 export default defineConfig({
-  // ...
   output: 'server',
-  adapter: vercel(),
+  adapter: netlify({}),
+  // Enable Preact to support Preact JSX components.
+  integrations: [preact()],
 });
